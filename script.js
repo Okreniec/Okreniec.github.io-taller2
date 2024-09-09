398f934b670730a6982386cd506222e9c7e2c9f6
Okreniec <103517069+Okreniec@users.noreply.github.com>
2024-09-09 19:54:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,17 @@
+function cambiarImagen() {
+    var img = document.getElementById('imgToChange');
+    if (img.src.includes('presiona-para-comenzar.png')) {
+        img.src = 'img/hola-como-estas.png';
+    } else if (img.src.includes('img/hola-como-estas.png')) {
+        img.src = 'img/soy-un-asistente-virtual.png';
+    } else if (img.src.includes('img/soy-un-asistente-virtual.png')) {
+        img.src = 'img/pero-que-es-un-asistente-virtual.png';
+    } else if (img.src.includes('pero-que-es-un-asistente-virtual.png')) {
+        window.location.href = 'popup00.html';
+    }   
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll(".item");
-    const portadaJuegos = document.querySelectorAll(".game");
-    const profileImages = document.querySelectorAll(".profileImage");
-    const imagesNoticias = document.querySelectorAll(".imageNoticia");
-
-    items.forEach((item, index) => {
-        const itemNumber = index + 1;
-        item.style.backgroundImage = `url(./imgs/Miniatura/Mini-${itemNumber}.png)`;
-    });
-
-    profileImages.forEach((profileImage, index) => {
-        const profileNumber = index + 1;
-        profileImage.style.backgroundImage = `url(./imgs/Profile//profile-${profileNumber}.png)`;
-    });
-
-    portadaJuegos.forEach((game, index) => {
-        const portadaNumber = index + 1;
-        game.style.backgroundImage = `url(./imgs/portadaJuego//portada-${portadaNumber}.png)`;
-    });
-
-    imagesNoticias.forEach((imageNoticia, index) => {
-        const profileNumber = index + 1;
-        imageNoticia.style.backgroundImage = `url(./imgs/Juegos//articulo-${profileNumber}.png)`;
-    });
-
-    const btnAbrirModal = 
-    document.querySelector("#btn-abrir-modal");
-    const btnCerrarModal = 
-    document.querySelector("#btn-cerrar-modal");
-    const btnCerrarModal1 = 
-    document.querySelector("#btn-cerrar-modal-1");
-    document.querySelector("#modal");
-
-    btnAbrirModal.addEventListener("click",()=>{
-        modal.showModal();
-    })
-
-    btnCerrarModal.addEventListener("click",()=>{
-        modal.close();
-    })
-
-    btnCerrarModal1.addEventListener("click",()=>{
-        modal.close();
-    })
-});
+window.onload = function() {
+    var img = document.getElementById('imgToChange');
+    img.onclick = cambiarImagen;
+};
